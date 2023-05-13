8a8536912f727740ad4b4fdfb24878326a023c79
Adriano Pavao <adribenig@hotmail.com>
2023-05-12 23:06:04 -0300

    retirada Mock PlayerService

--- src/test/java/tech/ada/games/jokenpo/controller/PlayerControllerTest.java
+++ src/test/java/tech/ada/games/jokenpo/controller/PlayerControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,9 +17,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tech.ada.games.jokenpo.dto.LoginDto;
 import tech.ada.games.jokenpo.dto.PlayerDto;
 import tech.ada.games.jokenpo.model.Player;
 import tech.ada.games.jokenpo.model.Role;
+import tech.ada.games.jokenpo.response.AuthResponse;
+import tech.ada.games.jokenpo.service.AuthService;
 import tech.ada.games.jokenpo.service.PlayerService;
 
 import java.util.Arrays;
@@ -26,19 +30,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PlayerControllerTest extends BaseControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
 
-    @MockBean
-    private PlayerService playerService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp() throws JsonProcessingException, Exception {
@@ -50,9 +50,9 @@ public class PlayerControllerTest extends BaseControllerTest {
     @Test
     public void testCreatePlayer() throws Exception {
 
-         String username = "usuario1";
+         String username = "usuario2";
          String password = "senha1";
-         String token = null;
+         String token = "usuario2";
 
         PlayerDto playerDto = new PlayerDto(username, password, token);
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/jokenpo/player/create")
@@ -61,21 +61,10 @@ public class PlayerControllerTest extends BaseControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
+
     @Test
     public void testFindPlayers() throws Exception {
 ;
-        Role role1 = new Role();
-        role1.setId(1L);
-        role1.setName("acess_1");
-        Set<Role> roles = new HashSet<>();
-        roles.add(role1);
-        Player player1 = new Player(1L, "John1", "12232", "John", roles);
-
-
-        Player player2 = new Player(1L, "Jane1", "12232", "Jane", roles);
-        List<Player> players = Arrays.asList(player1, player2);
-
-        Mockito.when(playerService.findPlayers()).thenReturn(players);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/jokenpo/player")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -83,30 +72,22 @@ public class PlayerControllerTest extends BaseControllerTest {
                 )
 
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("John"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("John1"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Jane"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].username").value("Jane1"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("usuario1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("usuario1"));
+
     }
 
     @Test
     public void testFindPlayer() throws Exception {
-        Role role1 = new Role();
-        role1.setId(1L);
-        role1.setName("acess_1");
-        Set<Role> roles = new HashSet<>();
-        roles.add(role1);
-        Player player = new Player(1L, "John1", "12232", "John", roles);
 
-        Mockito.when(playerService.findByPlayer(Mockito.anyString())).thenReturn(player);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/jokenpo/player/{player}", "John")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/jokenpo/player/{player}", "usuario1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .header("Authorization", this.getAuthorization())
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("John"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("John1"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("usuario1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("usuario1"));
     }
 
     @Test
